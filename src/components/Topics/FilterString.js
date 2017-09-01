6d75e7001a60275470890751b5cced898eb86952
tenndawg <nickolas.tenney@gmail.com>
2017-08-31 20:34:43 -0600

    update

---
+++ src/components/Topics/FilterString.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+
+class FilterString extends Component {
+    render() {
+        return(
+            <p>FilterString</p>        
+        )}
+}
+
+export default FilterString;
---
+++ src/components/Topics/Palindrome.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+
+class Palindrome extends Component {
+    render() {
+        return(
+            <p>Palindrome</p>
+        )}
+}
+
+export default Palindrome;
